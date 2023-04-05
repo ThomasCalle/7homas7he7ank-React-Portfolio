cbd0f92f1550109499b2f5e8357da8a8f83b6c49
Thomas Calle <thos.calle@gmail.com>
2023-04-05 15:12:34 -0400

    client side error react fails to lauch visible application

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,37 +1,37 @@
-// import React from 'react';
+import React from 'react';
 
-// function Project(props) {
-//   return (
-//     <div className="card">
-//       <a href={props.deployedLink} target="_blank" rel="noreferrer">
-//         <img
-//           src={props.imgSrc}
-//           className="card-img-top"
-//           alt={props.imgAlt}
-//         />
-//       </a>
-//       <div className="card-body">
-//         <h5 className="card-title">{props.title}</h5>
-//         <p className="card-text">{props.description}</p>
-//         <a
-//           href={props.deployedLink}
-//           target="_blank"
-//           rel="noreferrer"
-//           className="btn btn-primary"
-//         >
-//           Deployed App
-//         </a>
-//         <a
-//           href={props.githubLink}
-//           target="_blank"
-//           rel="noreferrer"
-//           className="btn btn-secondary"
-//         >
-//           GitHub Repo
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
+function Project(props) {
+  return (
+    <div className="card">
+      <a href={props.deployedLink} target="_blank" rel="noreferrer">
+        <img
+          src={props.imgSrc}
+          className="card-img-top"
+          alt={props.imgAlt}
+        />
+      </a>
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.description}</p>
+        <a
+          href={props.deployedLink}
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-primary"
+        >
+          Deployed App
+        </a>
+        <a
+          href={props.githubLink}
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-secondary"
+        >
+          GitHub Repo
+        </a>
+      </div>
+    </div>
+  );
+}
 
-// export default Project;
+export default Project;
