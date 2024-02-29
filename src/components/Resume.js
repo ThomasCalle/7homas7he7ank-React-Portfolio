34f287034e696f0100f8e0d9a294783823fa5e15
Thomas Calle <thos.calle@gmail.com>
2024-02-29 14:58:31 -0500

    updated resume and experience

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -20,8 +20,23 @@ function Resume() {
         </Row>
         <Row>
           <Col>
+          <hr className="star-secondary" />
+          <h3>Professional Summary</h3>
+            <div className="resume-item">
+              <ul>
+                <p>
+                  Highly qualified and motivated Full Stack Web Developer, recently graduated from the University of Toronto's Computer Science Bootcamp April 2023, seeking a junior developer or internship position to enhance my technical skills and understanding of project management principles. 
+                  With 14 years of demonstrated experience in business operations, customer service, and human resources, I specialize in merging technical expertise with superior interpersonal communication across all management levels. Dedicated to continuous learning and eager to contribute to and advance within your esteemed organization.  
+                </p>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <hr className="star-secondary" />
             <h3>Full Stack Development Skills</h3>
-            <hr className="star-secondary" />
+            {/* <hr className="star-secondary" /> */}
             <ul className="list-inline">
             <li className="list-inline-item">
                 <strong>Languages:</strong> HTML5, CSS3, JavaScript, Python, Scratch, #C </li>
@@ -45,58 +60,86 @@ function Resume() {
         </Row>
         <Row>
           <Col>
-            <h3>Education</h3>
             <hr className="star-secondary" />
+            <h3>Education</h3>
+            {/* <hr className="star-secondary" /> */}
             <div className="resume-item">
               <h4>Computer Science Degree in Software Development Spc. Full Stack Web Development</h4>
               <h5>University of Toronto April 2023</h5>
               <ul>
-              <li>Achievements: Top of class (4.0 GPA)</li>
-              <li>Projects: (20) Team Projects: (3)</li>
-              <li>Curriculum: HTML5, CSS3, JavaScript, Git, GitHub, MongoDB, Express.js, React.js, Node.js, Front-end, Back-end, Full Stack, MERN Stack, DOM, MVC, ORM, CLI, PWA’s, OOP, jQuery, Bootstrap, API, SQL, MySQL, GraphQL, SWE, SDLC, Agile Methodology (SCRUM & Kanban), UI/UX Design, Principles, Wireframe, Responsive Web Design & Web Accessibility.</li>
+                <li>Achievements: Top of class (4.0 GPA)</li>
+                <li>Projects: (20) Team Projects: (3)</li>
+                <li>Curriculum: HTML5, CSS3, JavaScript, Git, GitHub, MongoDB, Express.js, React.js, Node.js, Full Stack, MERN Stack, DOM, MVC, ORM, CLI, PWA’s, OOP, jQuery, Bootstrap, API, SQL, MySQL, GraphQL, SWE, SDLC, Agile Methodology (SCRUM & Kanban), UI/UX Design, Principles, Wireframe, Responsive Web Design & Web Accessibility.</li>
               </ul>
             </div>
           </Col>
         </Row>
         <Row>
           <Col>
-            <h3>Professional Experience</h3>
             <hr className="star-secondary" />
+            <h3>Professional Experience</h3>
+            {/* <hr className="star-secondary" /> */}
             <div className="resume-item">
-              <h4>Marketing & Branding, Model</h4>
-              <h5>Orange Model & Talent Management Inc. December 2022 – Present</h5>
-              <p>• Enhancing B2B partners' marketing and branding strategies amplifying national impact and reach of Creative Arts campaigns.
-                 • Championed Diversity and Inclusion (D&I) initiatives within PR modeling campaigns, actively promoting Equality & Representation.
-              </p>
+              <h4>Digital Marketing & Branding Manager</h4>
+              {/* <h5>Client @hasbulla.hushetskiy 2021 - Present</h5> */}
+              <h5>Client <a href="https://www.instagram.com/hasbulla.hushetskiy/?hl=en" target="_blank" rel="noopener noreferrer">@hasbulla.hushetskiy</a>| Toronto, Ontario December 2021 - Present</h5>
+              <ul>
+                <li>Project Link: <a href="https://www.instagram.com/ar/245529254340388/" target="_blank" rel="noopener noreferrer"> Hasbulla Instagram AR Filter</a></li>
+                <li>Developed and marketed "Hasbulla Filter" on Instagram and Facebook, generating over 16.4 million impressions and (335,000) shares, enhancing visibility</li>
+                <li>Achieved over (2) million views for the "Hasbulla Filter" within two months, markedly boosting engagement</li>
+                <li>Conducted market analysis, inform strategic decisions and achieving over 16.4 million impressions since Dec 2021</li>
+                <li>Partnered with B2B entities to improve SEO and AR, leading to a (4798.51%) surge in impressions, fostering positive community engagement on Meta and Discord</li>
+              </ul>
+            </div>
+            <div className="resume-item">
+              <h4>Marketing & Branding Model</h4>
+              <h5>Orange Model & Talent Management Inc. | Toronto, Ontario December 2022 - Present</h5>
+              <ul>
+                <li>Utilized 14 years of Customer Service and Business Operations, to improve B2B marketing/branding strategies, significantly expanding media campaign reach and impact.</li>
+                <li>Championed Diversity and Inclusion (D&I) initiatives within PR modeling campaigns, actively promoting Equality & Representation.</li>
+              </ul>
             </div>
             <div className="resume-item">
               <h4>Human Resources Director Assistant</h4>
-              <h5>Skills for Change May 2022 - November 2022</h5>
-              <p>
-                body
-              </p>
+              <h5>Skills for Change | Toronto, Ontario May 2022 - November 2022</h5>
+              <ul>
+              <li>Led HR department in Director's absence, ensuring policy compliance and proficient HRIS use</li>
+              <li>Resolved issues from part-time staff to executives, providing daily strategy reports to CEO.</li>
+              <li>Managed recruitment and onboarding for 58 employees across all levels, ensuring compliance and effective training.</li>
+              <li>Launched Mental Health & Wellness initiative with weekly "Wellness Wednesday" events, averaging 18+ employee attendance.</li>
+              {/* <li></li> */}
+              </ul>
             </div>
             <div className="resume-item">
-              <h4>Digital Marketing & Branding, AR Engagement Strategist</h4>
-              <h5>DELL CND Inc., Summer 2022-2023</h5>
-              <p>
-                body
-              </p>
+              <h4>Account Management Administration</h4>
+              <h5>Government of Ontario | Toronto, Ontario May 2022 - May 2023</h5>
+              <ul>
+                <li>Managed account and data entry for Covid-19 immunology records, enhancing corporate communication and customer service, and reducing partner discrepancies.</li>
+                <li>Exceeded regional health conflict resolutions by 51%, aiding 150-250 weekly reported illnesses.</li>
+              </ul>
             </div>
             <div className="resume-item">
-              <h4>Web Developer Intern.</h4>
-              <h5>DELL CND Inc., Summer 2022-2023</h5>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                cursus bibendum magna, vitae dapibus felis ultricies ut. Sed ac
-                odio eget est convallis ullamcorper. Duis bibendum vestibulum
-                risus, non convallis odio aliquam eu.
-              </p>
+              <h4>Management, Supervisor</h4>
+              <h5>Liberty Entertainment Group Ltd. | Toronto, Ontario May 2017 - October 2019 </h5>
+              <ul>
+                  <li>Enhanced HR and payroll efficiency using ADP Software, managing over 100 employees and reducing discrepancies.</li>
+                  <li>Improved recruitment and onboarding with a personalized mentorship program, streamlining integration and increasing workplace confidence.</li>
+              </ul>
             </div>
+            <h4>Merchandise & Retail Sales Associate Lead</h4>
+            <h5>Maple Leaf Sports & Entertainment Ltd. (MLSE) | Toronto, Ontario March 2011 - July 2017 </h5>
+              <ul>
+                <li>Progressed through four roles due to exceptional performance in customer relations and team management.</li>
+                <li>Led a team of 5+, enhancing organizational development and teamwork to meet sales KPIs.</li>
+                <li>Conducted 16+ successful seasonal training sessions, improving recruitment and employee performance.</li>
+                <li>Achieved highest store sales, increasing units-per-transaction from 1.8 to 2.3 during major events.</li>
+                <li>Received 15+ Customer Service Awards and 2 Corporate-wide Employee of the Year Awards.</li>
+              </ul>   
           </Col>
         </Row>
         <Row>
           <Col>
+            <hr className="star-secondary" />
             <h3>Proficiencies</h3>
             {/* <ul>
               <li>HTML/CSS</li>
